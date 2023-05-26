341ea659d71c41b4f129f740785cf28510f902ec
harish242 <120125264+harish242@users.noreply.github.com>
2023-05-26 17:25:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const[mass,setMass]=useState(false)
+  const addPara=()=>{
+    setMass(true)
+  }
   return (
     <div id="main">
       // Do not alter the main div
+      {mass&&<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+      <button id="click" onClick={addPara}>click</button>
     </div>
   );
 }
